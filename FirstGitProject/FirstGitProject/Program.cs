5ef14c445395f8fc1855a1917b62589bfdf848d6
TomasG <tomijus7@gmail.com>
2019-12-12 20:03:12 +0200

    Function 'TestNumberInRange' addes

--- FirstGitProject/FirstGitProject/Program.cs
+++ FirstGitProject/FirstGitProject/Program.cs
@@ -7,15 +7,19 @@ namespace FirstGitProject
         static void Main()
         {
             Console.WriteLine(Max(10, 6, 15));
+            Console.WriteLine(TestNumberInRange(5, 0, 10));
+            Console.WriteLine(TestNumberInRange(-5, 0, 10));
+            Console.WriteLine(TestNumberInRange(5, 10, 0));
+            Console.WriteLine(TestNumberInRange(5, -5, 10));
         }
 
         static int Max(int a, int b, int c)
         {
-            if (a>b && a>c)
+            if (a > b && a > c)
             {
                 return a;
             }
-            else if (b>c)
+            else if (b > c)
             {
                 return b;
             }
@@ -23,6 +27,14 @@ namespace FirstGitProject
             {
                 return c;
             }
+        }
+        static bool TestNumberInRange(int testNumber, int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                Console.WriteLine("Error. Bad range values.");
+            }
+            return testNumber >= minValue && testNumber <= maxValue;
         }
     }
 }
