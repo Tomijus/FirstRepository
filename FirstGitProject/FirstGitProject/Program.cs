424a18478ae546366fd9cd3c29bff133ae94c65b
TomasG <tomijus7@gmail.com>
2019-12-12 19:48:19 +0200

    Added 'Max' function.

--- FirstGitProject/FirstGitProject/Program.cs
+++ FirstGitProject/FirstGitProject/Program.cs
@@ -4,9 +4,25 @@ namespace FirstGitProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Max(10, 6, 15);
+        }
+
+        static void Max(int a, int b, int c)
+        {
+            if (a>b && a>c)
+            {
+                Console.WriteLine($"Didziausias yra {a}");
+            }
+            else if (b>a && b>c)
+            {
+                Console.WriteLine($"Didziausias yra {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Didziausias yra {c}");
+            }
         }
     }
 }
